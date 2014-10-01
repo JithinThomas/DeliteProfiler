79a86506361a927874798cdf723fa79318ebf6dd
Jithin Thomas <jithinpt@rescomp-13-273754.stanford.edu>
2014-09-30 19:17:54 -0700

    Uploading current version

---
+++ common.js
@@ -0,0 +1,10 @@
+
+// Convert 'name' to HTML class selector string
+function toClassSelector(name) {
+	return '.' + name;
+}
+
+// Convert 'name' to HTML id selector string
+function toIdSelector(name) {
+	return '#' + name;
+}
